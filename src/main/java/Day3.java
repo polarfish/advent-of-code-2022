3f5070b8ce9d8bdf76246aa427133c611e10f616
Eugene Grybinnyk <eugeneg@backbase.com>
2022-12-03 08:32:17 +0100

    Day 3

---
+++ src/main/java/Day3.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day3 extends Day {
+
+    Day3() {
+        super(3, "Sum priorities misplaced types", "Sum priorities badge types");
+    }
+
+    public static void main(String[] args) {
+        new Day3().solve();
+    }
+
+    @Override
+    public long part1(String input) {
+        return Arrays.stream(input.split("\n"))
+            .map(this::findMisplacedItem)
+            .mapToInt(this::calculatePriority)
+            .sum();
+    }
+
+
+    public long part2(String input) {
+        String[] rucksacks = input.split("\n");
+
+        long total = 0;
+        for (int i = 0; i < rucksacks.length; i += 3) {
+            total += calculatePriority(
+                findBadgeItem(List.of(rucksacks[i], rucksacks[i + 1], rucksacks[i + 2])));
+        }
+
+        return total;
+    }
+
+    private Character findMisplacedItem(String itemsList) {
+        int len = itemsList.length();
+        var left = new HashSet<Character>();
+        var right = new HashSet<Character>();
+        for (int i = 0; i < len / 2; i++) {
+            left.add(itemsList.charAt(i));
+        }
+        for (int i = len / 2; i < len; i++) {
+            right.add(itemsList.charAt(i));
+        }
+
+        left.retainAll(right);
+
+        return left.iterator().next();
+    }
+
+    private Character findBadgeItem(List<String> itemsLists) {
+        return itemsLists.stream()
+            .map(String::toCharArray)
+            .map(itemsArray -> {
+                Set<Character> itemsSet = new HashSet<>();
+                for (Character item : itemsArray) {
+                    itemsSet.add(item);
+                }
+                return itemsSet;
+            })
+            .reduce((s1, s2) -> {
+                s1.retainAll(s2);
+                return s1;
+            }).orElseThrow().iterator().next();
+    }
+
+    int calculatePriority(Character item) {
+        if (Character.isUpperCase(item)) {
+            return item - 38;
+        } else {
+            return item - 96;
+        }
+    }
+}
