83e5ec6ab3bbc81d240ac5f893f18a59cd7a96ed
Eugene Grybinnyk <eugeneg@backbase.com>
2022-12-06 10:27:21 +0100

    Added AllDaysRunner

---
+++ src/main/java/AllDaysRunner.java
@@ -0,0 +1,11 @@
+public class AllDaysRunner {
+    public static void main(String[] args) {
+        new Day1().run();
+        new Day2().run();
+        new Day3().run();
+        new Day4().run();
+        new Day5().run();
+        new Day6().run();
+    }
+
+}
