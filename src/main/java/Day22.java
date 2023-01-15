be867517265534817aa76952ca8d51e9c4b690cd
Eugene Grybinnyk <eugeneg@backbase.com>
2023-01-15 09:07:01 +0100

    Day 22 part 2 initial solution

--- src/main/java/Day22.java
+++ src/main/java/Day22.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Day22 extends Day {
 
     public static void main(String[] args) {
-        Day22 day = new Day22();  // https://adventofcode.com/2022/day/0
+        Day22 day = new Day22();  // https://adventofcode.com/2022/day/22
 
         String sample = readFile("%s_sample.txt".formatted(day.name()));
         String full = readFile("%s.txt".formatted(day.name()));
@@ -11,13 +14,48 @@ public class Day22 extends Day {
         assertEquals(6032, day.part1(sample));
         assertEquals(88226, day.part1(full));
 
-//        assertEquals(5031, day.part2(sample));
-//        assertEquals(0, day.part2(full));
+        assertEquals(5031, day.part2(sample));
+        assertEquals(57305, day.part2(full));
 
         day.run(full, day::part1, "Part 1 result");
-//        day.run(full, day::part2, "Part 2 result");
+        day.run(full, day::part2, "Part 2 result");
     }
 
+    private static final int X = 0;
+    private static final int Y = 1;
+    private static final int DIR = 2;
+
+    private static final Map<String, List<Wrapper>> LAYOUTS = Map.of(
+        "4-2456ab", List.of(
+            new Wrapper(8, 0, 11, 0, 3, pos -> new int[]{11 - pos[X], 4, 1}),
+            new Wrapper(11, 4, 11, 7, 0, pos -> new int[]{19 - pos[Y], 8, 1}),
+            new Wrapper(8, 11, 11, 11, 1, pos -> new int[]{11 - pos[X], 7, 3}),
+            new Wrapper(4, 4, 7, 4, 3, pos -> new int[]{8, pos[X] - 4, 0})
+        ),
+        "50-12589c", List.of(
+            // light green
+            new Wrapper(50, 0, 99, 0, 3, pos -> new int[]{0, pos[X] + 100, 0}),
+            new Wrapper(0, 150, 0, 199, 2, pos -> new int[]{pos[Y] - 100, 0, 1}),
+            // green
+            new Wrapper(49, 150, 49, 199, 0, pos -> new int[]{pos[Y] - 100, 149, 3}),
+            new Wrapper(50, 149, 99, 149, 1, pos -> new int[]{49, pos[X] + 100, 2}),
+            // blue
+            new Wrapper(0, 100, 49, 100, 3, pos -> new int[]{50, pos[X] + 50, 0}),
+            new Wrapper(50, 50, 50, 99, 2, pos -> new int[]{pos[Y] - 50, 100, 1}),
+            // purple
+            new Wrapper(99, 100, 99, 149, 0, pos -> new int[]{149, 149 - pos[Y], 2}),
+            new Wrapper(149, 0, 149, 49, 0, pos -> new int[]{99, 149 - pos[Y], 2}),
+            // pink
+            new Wrapper(100, 0, 149, 0, 3, pos -> new int[]{pos[X] - 100, 199, 3}),
+            new Wrapper(0, 199, 49, 199, 1, pos -> new int[]{pos[X] + 100, 0, 1}),
+            // orange
+            new Wrapper(100, 49, 149, 49, 1, pos -> new int[]{99, pos[X] - 50, 2}),
+            new Wrapper(99, 50, 99, 99, 0, pos -> new int[]{pos[Y] + 50, 49, 3}),
+            // red
+            new Wrapper(0, 100, 0, 149, 2, pos -> new int[]{50, 149 - pos[Y], 0}),
+            new Wrapper(50, 0, 50, 49, 2, pos -> new int[]{0, 149 - pos[Y], 0})
+        )
+    );
 
     @Override
     public String part1(String input) {
@@ -44,37 +82,159 @@ public class Day22 extends Day {
         for (int i = 0; i < instructions.length(); i++) {
             char nextChar = instructions.charAt(i);
             if (nextChar == 'L') {
-                move(map, pos, steps);
+                moveFlat(map, pos, steps);
                 steps = 0;
-                pos[2] = ((pos[2] - 1) + 4) % 4;
+                pos[DIR] = ((pos[DIR] - 1) + 4) % 4;
             } else if (nextChar == 'R') {
-                move(map, pos, steps);
+                moveFlat(map, pos, steps);
                 steps = 0;
-                pos[2] = (pos[2] + 1) % 4;
+                pos[DIR] = (pos[DIR] + 1) % 4;
             } else {
                 steps = steps * 10 + (nextChar - 48);
             }
         }
-        move(map, pos, steps);
-
-//        printMap(map, pos);
-
+        moveFlat(map, pos, steps);
 
         return String.valueOf(
-            (pos[1] + 1) * 1000 + (pos[0] + 1) * 4 + pos[2]
+            (pos[Y] + 1) * 1000 + (pos[X] + 1) * 4 + pos[DIR]
         );
     }
 
     @Override
     public String part2(String input) {
-        return "0";
+        String[] lines = input.split("\n");
+        String[] mapLines = Arrays.copyOfRange(lines, 0, lines.length - 2);
+        String instructions = lines[lines.length - 1];
+        int mapSize = Math.max(
+            Arrays.stream(mapLines).mapToInt(String::length).max().orElseThrow(),
+            mapLines.length
+        );
+
+        char[][] map = new char[mapSize][mapSize];
+
+        int[] pos = null;
+        for (int y = 0; y < mapSize; y++) {
+            if (y >= mapLines.length) {
+                Arrays.fill(map[y], ' ');
+                continue;
+            }
+
+            for (int x = 0; x < mapSize; x++) {
+                map[y][x] = x >= mapLines[y].length()
+                    ? ' '
+                    : mapLines[y].charAt(x);
+                if (pos == null && y == 0 && map[y][x] == '.') {
+                    pos = new int[]{x, y, 0};
+                }
+            }
+        }
+
+        // determine cube layout
+        int cubeSide = mapSize / 4;
+        StringBuilder sb = new StringBuilder().append(cubeSide).append('-');
+        for (int i = 0; i < 16; i++) {
+            int x = (i % 4) * cubeSide;
+            int y = (i / 4) * cubeSide;
+            if (map[y][x] != ' ') {
+                sb.append("0123456789abcdef".charAt(i));
+            }
+        }
+
+        String layout = sb.toString();
+
+        int steps = 0;
+        for (int i = 0; i < instructions.length(); i++) {
+            char nextChar = instructions.charAt(i);
+            if (nextChar == 'L') {
+                moveCubic(layout, map, pos, steps);
+                steps = 0;
+                pos[DIR] = ((pos[DIR] - 1) + 4) % 4;
+            } else if (nextChar == 'R') {
+                moveCubic(layout, map, pos, steps);
+                steps = 0;
+                pos[DIR] = (pos[DIR] + 1) % 4;
+            } else {
+                steps = steps * 10 + (nextChar - 48);
+            }
+        }
+        moveCubic(layout, map, pos, steps);
+
+        return String.valueOf(
+            (pos[Y] + 1) * 1000 + (pos[X] + 1) * 4 + pos[DIR]
+        );
     }
 
-    void move(char[][] map, int[] pos, int steps) {
-        int x = pos[0];
-        int y = pos[1];
+    record Wrapper(int x1, int y1, int x2, int y2, int dir,
+                   Function<int[], int[]> wrapFunction) {
+
+        boolean applies(int[] pos) {
+            return pos[DIR] == dir & pos[X] >= x1 && pos[X] <= x2 && pos[Y] >= y1 && pos[Y] <= y2;
+        }
+    }
+
+    void moveCubic(String layout, char[][] map, int[] pos, int steps) {
+        int[] newPos;
         for (int i = 0; i < steps; i++) {
-            switch (pos[2]) {
+            int x = pos[X];
+            int y = pos[Y];
+            switch (pos[DIR]) {
+                case 0 -> {
+                    if (x == map[0].length - 1 || map[pos[Y]][x + 1] == ' ') {
+                        newPos = wrapCubic(layout, pos);
+                    } else {
+                        newPos = Arrays.copyOf(pos, 3);
+                        newPos[X]++;
+                    }
+                }
+                case 1 -> {
+                    if (y == map.length - 1 || map[y + 1][x] == ' ') {
+                        newPos = wrapCubic(layout, pos);
+                    } else {
+                        newPos = Arrays.copyOf(pos, 3);
+                        newPos[Y]++;
+                    }
+
+                }
+                case 2 -> {
+                    if (x == 0 || map[y][x - 1] == ' ') {
+                        newPos = wrapCubic(layout, pos);
+                    } else {
+                        newPos = Arrays.copyOf(pos, 3);
+                        newPos[X]--;
+                    }
+                }
+                case 3 -> {
+                    if (y == 0 || map[y - 1][x] == ' ') {
+                        newPos = wrapCubic(layout, pos);
+                    } else {
+                        newPos = Arrays.copyOf(pos, 3);
+                        newPos[Y]--;
+                    }
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + pos[DIR]);
+            }
+
+            if (map[newPos[Y]][newPos[X]] == '#') {
+                return;
+            }
+
+            System.arraycopy(newPos, 0, pos, 0, 3);
+        }
+    }
+
+    int[] wrapCubic(String layout, int[] pos) {
+        return LAYOUTS.get(layout).stream()
+            .filter(wrapper -> wrapper.applies(pos))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Missing wrap function for " + Arrays.toString(pos)))
+            .wrapFunction.apply(pos);
+    }
+
+    void moveFlat(char[][] map, int[] pos, int steps) {
+        int x = pos[X];
+        int y = pos[Y];
+        for (int i = 0; i < steps; i++) {
+            switch (pos[DIR]) {
                 case 0 -> {
                     if (x == map[0].length - 1 || map[y][x + 1] == ' ') {
                         x = wrapLtrX(map, y);
@@ -84,7 +244,7 @@ public class Day22 extends Day {
                     if (map[y][x] == '#') {
                         return;
                     } else {
-                        pos[0] = x;
+                        pos[X] = x;
                     }
                 }
                 case 1 -> {
@@ -96,7 +256,7 @@ public class Day22 extends Day {
                     if (map[y][x] == '#') {
                         return;
                     } else {
-                        pos[1] = y;
+                        pos[Y] = y;
                     }
                 }
                 case 2 -> {
@@ -108,7 +268,7 @@ public class Day22 extends Day {
                     if (map[y][x] == '#') {
                         return;
                     } else {
-                        pos[0] = x;
+                        pos[X] = x;
                     }
                 }
                 case 3 -> {
@@ -120,7 +280,7 @@ public class Day22 extends Day {
                     if (map[y][x] == '#') {
                         return;
                     } else {
-                        pos[1] = y;
+                        pos[Y] = y;
                     }
                 }
             }
@@ -163,16 +323,4 @@ public class Day22 extends Day {
         return -1;
     }
 
-    void printMap(char[][] map, int[] pos) {
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (pos[0] == x && pos[1] == y) {
-                    System.out.print('@');
-                } else {
-                    System.out.print(map[y][x]);
-                }
-            }
-            System.out.println();
-        }
-    }
 }
